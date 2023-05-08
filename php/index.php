1502694db6d8f53db82970027f24f699516fc59e
mehmet020 <mehmetzakelijk020@gmail.com>
2023-05-08 10:27:25 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+<?php
+$var1 = 12;
+$var2 = 10;
+$sum = $var1 + $var2;
+echo $sum;
+?>
+<br>
+<?php
+$var1 = 12;
+$var2 = 10;
+$product = $var1 * $var2;
+echo $product;
+?>
+<br>
+<?php
+$name = "Mehmet";
+echo "Welkom " . $name . "!";
+?>
+<br>
+
+<?php
+
+/*  deze code bevat veel fouten
+ haal alle fouten eruit!
+*/
+
+$mijnNaam = "Mehmet";
+$jouwNaam = "Omer";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
+
+</body>
+</html>
+
