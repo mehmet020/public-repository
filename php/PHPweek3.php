7a3a503da2c080f9e257f65ea22b58899c18b4c0
mehmet020 <131659237+mehmet020@users.noreply.github.com>
2023-05-25 23:25:35 +0200

    Add files via upload

---
+++ php/PHPweek3.php
@@ -0,0 +1,40 @@
+<!-- delete -->
+<?php
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+// $myArray[] = "tram";
+
+// echo $myArray[6];
+?>
+<!-- deel2 -->
+<?php
+// $aantalElementen = count($myArray);
+// echo "het array heeft" . $aantalElementen . "elementen";
+// $myArray[] = 'boot';
+// $aantalElementen = count($myArray);
+// echo "het array heeft" . $aantalElementen . "elementen";
+?>
+deel 3
+<?php
+// op regel 2 word foo afgedrukt
+// op regel 5 word 2 afgedrukt
+// op regel 8 word BMW afgedrukt
+// op regel 9 word 3 getelt
+// op regel 12 word 5 afgedrukt
+// op regel 19 word  er getelt tot 4
+?>
+
+<?php
+//deel4a
+$cijfersphp = [4.4, 4.6, 5.6, 6., 7.6, 7.2];
+
+$gemiddelde = round(array_sum($cijfersphp) / count($cijfersphp), 1);
+
+echo "gemiddelde is: ".$gemiddelde;
+
+
+//deel4b
+
+//Ik denk dat het beter is om de regels samen te voegen, omdat het de code vereenvoudigt en de leesbaarheid verbetert
+
+?>
+
